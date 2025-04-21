8d873a44973aa4d19b4317bc0be4de01d0fc66d9
demosthenesd <demosthenesd@icloud.com>
2025-04-21 21:35:45 +1000

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,6 @@ const app = express();
 const morgan = require('morgan')
 const Person = require('./models/person');
 
-
-
-
 morgan.token('body', req => {
     return JSON.stringify(req.body)
 })
@@ -16,6 +13,19 @@ app.use(morgan(':method :url :status  :response-time ms   :body'))
 
 app.use(cors());
 app.use(express.static('dist'))
+
+
+const errorHandler = (error, request, response, next) => {
+    console.error(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({ error: 'malformatted id' })
+    }
+    else {
+        console.log(error)
+    }
+
+    next(error)
+}
 
 
 morgan.token('type', function (req, res) { return req.headers['content-type'] })
@@ -33,25 +43,25 @@ app.get("/api/info", async (req, res) => {
     console.log(Person.find({}).then(person => { console.log(person.length) }));
     const numOfPersons = await Person.find({}).then(person => { return person.length })
     const htmlRes = `<p> Phonebook has info for ${numOfPersons} people</p>
-                    <p>${now.toString()}
+    <p>${now.toString()}
     `
     res.send(htmlRes)
 })
 
 
 
-app.get("/api/persons/:id", (req, res) => {
+app.get("/api/persons/:id", (req, res, next) => {
 
     Person.findById(req.params.id).then(person => {
-        response.json(person)
-    })
+        res.json(person)
+    }).catch(error => next(error))
+
 })
 
 
-app.delete("/api/persons/:id",  (req, res) => {
-
+app.delete("/api/persons/:id", (req, res, next) => {
     Person.deleteOne({ _id: req.params.id }).then(output =>
-        res.json(output))
+    res.json(output)).catch(error => next(error))
 })
 
 
@@ -74,16 +84,40 @@ app.post("/api/persons", (req, res) => {
     });
 
     newPerson.save()
-        .then(savedPerson => {
-            res.json(savedPerson);
-        })
-        .catch(error => {
-            console.error("Error saving person:", error.message);
-            res.status(500).json({ error: "Failed to save person" });
-        });
+    .then(savedPerson => {
+        res.json(savedPerson);
+    })
+    .catch(error => {
+        next(error)
+    });
 });
 
+app.put('/api/persons/:id', (request, response, next) => {
+    const { name, number } = request.body
 
+    Person.findById(request.params.id)
+    .then(person => {
+        if (!person) {
+            return response.status(404).end()
+        }
+
+
+        person.name = name
+        person.number = number
+
+        return person.save().then((updatedPerson) => {
+            response.json(updatedPerson)
+        })
+    })
+    .catch(error => next(error))
+})
+
+
+
+
+
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log("listening to port: ", PORT))
